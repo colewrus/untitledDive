166915da160c0a6c3fff9a622e8af4cebb0470c2
Cole Wrampelmeier <wrampelm@american.edu>
2018-01-21 11:57:50 -0500

    first commit with stuph

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+
+    Animator ani;
+    public bool inWater;
+
+	// Use this for initialization
+	void Start () {
+        ani = this.GetComponent<Animator>();
+        inWater = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        EnterWater();
+		
+	}
+
+
+    void EnterWater()
+    {
+        if (!inWater)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("hit space " + ani.GetBool("goJump")); 
+                //play jump animation
+                ani.SetBool("goJump", true);
+            }
+        }
+    }
+
+
+
+}
